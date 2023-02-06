6492980bd387e35d48f1b496d818cb8c99ecc2a9
AzarovDima <ada89885308140@gmail.com>
2023-02-06 23:55:45 +0300

    Аккерман

--- Recurs_3_68_Akkerman/Program.cs
+++ Recurs_3_68_Akkerman/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int numberM = GetNumberFromUser("Введите натуральное число M: ", "Ошибка ввода попробуйте еще раз!");
+int numberN = GetNumberFromUser("Введите натуральное число M: ", "Ошибка ввода попробуйте еще раз!");
+double result = AckermannFunction(numberM, numberN);
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {result}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        if (isCorrect)
+            return number;
+        else
+            Console.WriteLine(errorMessage);
+    }
+
+
+}
+double AckermannFunction(double m, double n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    return AckermannFunction(m, n);
+}
